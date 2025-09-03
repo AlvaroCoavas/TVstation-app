53b53ea2848ed014fb4956c6d07a8de68ed6f4cb
AlvaroCoavas <coavascantero@gmail.com>
2025-09-03 16:36:39 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TVstation-app',
+  webDir: 'www'
+};
+
+export default config;
